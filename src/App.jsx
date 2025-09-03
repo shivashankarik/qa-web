0577fe86614acf7614a8d023138a1f30a0537e18
shivashankarik <demo96637@gmail.com>
2025-09-03 16:17:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Welcome to QA Web 🚀</h1>
+            <h1>Welcome to QA Web Good Morning 🚀</h1>
             <p>This is a sample React project deployed with Jenkins + AWS S3.</p>
         </div>
     )
